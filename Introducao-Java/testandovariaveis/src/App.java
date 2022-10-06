ca2b1f0229ed5167cf3ca4951fe235b887f2cba8
jkirst <joaoalekirst@gmail.com>
2022-10-06 11:37:49 -0300

    Segundo Commit Teste de Variáveis

--- Introducao-Java/testandovariaveis/src/App.java
+++ Introducao-Java/testandovariaveis/src/App.java
@@ -3,12 +3,36 @@ public class App {
         int i;
        // não pode ser utilizado uma variavel com o mesmo nome int i;
         int I;
-        // invalido int _1a;
+        // invalido começando com letras int 1a;
+        int _1a;
         int $aq;
 
         i = 5;
         I = 10;
         _1a = 20;
         $aq = 7;
+
+        final int j = 10;
+       // Como é um valor final não pode ser modificado  
+       //j = 15;
+        int asrn24678md;
+       // Não é  permitido espaço nas variaveis
+       //int asrn2$4678 _md = 10;
+
+       // não é uma boa prática usar % 
+       // int asrn2$46%78_md = 10;
+
+        asrn24678md = 100;
+        //asrn2$4678_md = 10;
+
+        int quantidadeProduto = 50;
+        int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        final int numeroTentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        int qtdProd;
+
+        System.out.println(i);
+      
     }
 }
