f7cc45ea6f9b6f01d6ca2cb6e94101d8f1ee893b
jkirst <joaoalekirst@gmail.com>
2022-10-06 11:23:34 -0300

    Exercício Testar Variáveis Java

---
+++ Introducao-Java/testandovariaveis/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int i;
+       // não pode ser utilizado uma variavel com o mesmo nome int i;
+        int I;
+        // invalido int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+    }
+}
